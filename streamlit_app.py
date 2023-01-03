12a1251c86c5400ed9bbb4ee0f690bfa5711499d
shivamandadi <121796187+shivamandadi@users.noreply.github.com>
2023-01-03 10:26:13 +0530

    Update streamlit_app.py
    fixed the error

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title('fisrst streamlit file')
 streamlit.header('open the new file')
-streamli.text('tryin to create a new one for streamlit')
+streamlit.text('tryin to create a new one for streamlit')
