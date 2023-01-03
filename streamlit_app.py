039c6bdbf9eab1d9d0f46ae296c474f87ada10ac
shivamandadi <121796187+shivamandadi@users.noreply.github.com>
2023-01-03 10:25:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title('fisrst streamlit file')
 streamlit.header('open the new file')
-streamlit.text('tryin to create a new one for streamlit')
+streamli.text('tryin to create a new one for streamlit')
